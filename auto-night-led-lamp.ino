0c1e54d092e6ac5ded0aac052948b4bced630f1d
NerveClasp <nerveclasp@gmail.com>
2016-11-09 01:52:38 +0200

    Made a version that turns on when you interfere with one of the sensors and turns off if you interfere with one of the sensors in three seconds or more.

--- auto-night-led-lamp.ino
+++ auto-night-led-lamp.ino
@@ -1,9 +1,13 @@
 /*
- * So we have two sensors. This version of the software for the Arduino works in the following way:
+ * So we have two sensors. This branch works in the following way:
  * Whenever any of the two triggers can see an object closer than 150 cm, it fires up both led lamps and they
  * are lit until there is no interference with the sensor.
  * The sensor is cecking for an object every second. Every time there is no interference the idleCounter increments.
  * If the idleCounter becomes more than wait, the sensor starts checking for the interference less frequetly to save up some energy.
+ * Led lamps will stay lit until one of the sensors will be triggered again, meaning if you go in and one of the sensors is detecting that,
+ * once you go out using any door, the ligts will turn off in delayTime seconds. If none of the sensors are triggered once you go in,
+ * the lights will turn off in maxLife seconds (to prevent lamps from being on the whole night if none of the sensors detected you
+ * leaving for some reason).
  */
 
 int inFirst = 8;//first sensor's echo pin
@@ -14,10 +18,11 @@ int pingSecond = 11;//second sensor's trig pin
 
 int ledFirst = 12;//first led's power pin
 int ledSecond = 13;//second led's power pin
-String inFirst, inSecond = "out"; 
+String inOut = "out"; //stores whether the sensor delected you coming in or out.
 
 int idleCounter, afterLife = 0;
-int maxLife = 20;
+int delayTime = 20;
+int maxLife = 600;
 double wait = 120000;
 
 void setup() {
@@ -31,7 +36,7 @@ void setup() {
   digitalWrite(pingFirst, LOW);
   digitalWrite(pingFirst, LOW);
 }
- 
+
 void loop() {
   long duration, cmFirst, cmSecond,tmp1;
   //sending the first sensor's signal (high level for 10 mcs)
@@ -51,7 +56,7 @@ void loop() {
   //counting the time it took for the signal to come back
   duration=micros()-tmp1;
   cmFirst = microsecondsToCentimeters(duration);//converting the time into santimeters
-  
+
   Serial.print("Distance: ");
   //do the same for the second sensor
   digitalWrite(pingSecond, HIGH);
@@ -65,36 +70,23 @@ void loop() {
   }
   duration=micros()-tmp1;
   cmSecond = microsecondsToCentimeters(duration);
-  /*
-   * this is for the main branch, will detele later (still new to all this git stuff, sorry))
-  if(cmFirst < 150 && inSecond != "in"){
-    inFirst = "in";
-  }else if (cmFirst < 150){
-    inFirst = "out";
-  }
-  if(cmSecond < 150 && inFirst != "in"){
-    inSecond = "in";
-  }else if (cmSecond < 150){
-    inSecond = "out";
-  }
-  
-  if(cmSecond < 150){
-    inSecond = true;
-  }
-  */
   if(cmFirst < 150 || cmSecond < 150){
-    idleCounter = 0;
-    Serial.println(cmSecond);
-    Serial.println("Turning on the lights");
-    analogWrite(ledFirst, 255);
-    analogWrite(ledSecond, 255);
-    afterLife = maxLife;
-    
-    
-    delay(1000);
+    if(idleCounter > 2){
+      if(inOut == "out"){
+        inOut = "in";
+        Serial.println("Turning on the lights");
+        analogWrite(ledFirst, 255);
+        analogWrite(ledSecond, 255);
+      }else{
+        inOut = "out";
+      }
+    }
+    afterLife = delayTime; //
   }else{
-//    if(afterLife == 0 && inFirst == "out" || inSecond == "out"){
-    if(afterLife == 0){
+    if(idleCounter > maxLife){
+      inOut = "out";
+    }
+    if(afterLife == 0 && inOut == "out"){
       analogWrite(ledFirst, 0);
       analogWrite(ledSecond, 0);
     }
@@ -103,18 +95,15 @@ void loop() {
     idleCounter++;
     delay(1000);
   }
-  if(afterLife != 0){
+  if(afterLife != 0 && inOut == "out"){
     afterLife--;
   }
   if(idleCounter > wait){
     delay(120000);
   }
 }
-long microsecondsToCentimeters(long microseconds)
-{
+
+long microsecondsToCentimeters(long microseconds) {
   //the speed of the sound is 340 m/s or 29412 microseconds/sm, and hence the sound travels to the obstacle and back, we divide it by two
   return microseconds / 29.412 / 2;
 }
-
-
-
